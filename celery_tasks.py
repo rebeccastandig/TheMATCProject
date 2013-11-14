f014f63f6fa3aa75c6470b3b6f46ad1278269d0f
Rebecca Standig <rebeccastandig@gmail.com>
2013-11-14 13:33:13 -0800

    Refactored code for cleanliness and repetition.

--- celery_tasks.py
+++ celery_tasks.py
@@ -1,5 +1,4 @@
 from celery import Celery
-from wordnik import *
 import model
 import wordnik_api
 import tweet_cleaner
@@ -12,54 +11,24 @@ def parse_tweets(tweet):
 		keep_tweet = tweet
 		tweet = tweet[0].split(' ')
 		cleaned_tweet = tweet_cleaner.clean_tweet(tweet)
-		length = 0
 		words = []
-		meta = ['RT', 'RTs', 'RT\'s', 'R/T', 'rt', 'rt\'s', 'rts', 'r/t', 'HT', 'H/T','ht', 'h/t', 'MT', 'M/T', 'mt', 'm/t', 'CR', 'C/R', 'cr', 'c/r']
-		non_words = ['jeje', 'haha', 'mwah', 'bwah', 'muah', 'buah', 'http', 'teeh', 'jeej']
-		beg_num= ['@', '$', '#', ':', ';', '>', '^','0','1','2','3','4','5','6','7','8','9']
-		while length < len(cleaned_tweet)-1:
-			for word in cleaned_tweet:
-				# stripping word twice to account for people's weird punctuation errors
-				word = tweet_cleaner.strip_word(word)
-				word = tweet_cleaner.strip_word(word)
-				if len(word) > 0:
-					if word[0] in beg_num:
-						word = 0
-						length += 1
-					elif word in meta:
-						word = 0
-						length += 1
-					elif word[:4] in non_words:
-						word = 0
-						length += 1
-					elif len(word) < 2:
-						word = 0
-						length += 1
-					elif '&' in word:
-						word = 0
-						length += 1
-					elif ':-D' in word:
-						# do this for 0.0 o.o O.O -_- :-P :-) ;-P ;-) etc too
-						word = 0
-						length += 1
-					elif len(word) > 0:
-						if word[-1] == 's' and word[-2] == '\'':
-							word = 0
-							length += 1
-						if word != 0:
-							for letter in word:
-								if ord(letter) > 128:
-									word = 0
-									length += 1
-					elif len(word) > 1:
-						if word[1] in beg_num:
-							word = 0
-							length += 1
-					length += 1
-					words.append(word)
-	words_in_game = []
-	if len(words) > 0:
+		final_words = []
+		for word in cleaned_tweet:
+			if len(word) > 2:
+				checking_words = tweet_cleaner.check_words(word)
+				if checking_words != 0:
+					words.append(checking_words)
 		for word in words:
+			# stripping word twice to account for people's weird punctuation errors
+			word = tweet_cleaner.strip_word(word)
+			word = tweet_cleaner.strip_word(word)
+			if len(word) > 2:
+				checking_words = tweet_cleaner.check_words(word)
+				if checking_words != 0:
+					final_words.append(checking_words)
+	words_in_game = []
+	if len(final_words) > 0:
+		for word in final_words:
 			if word > 0:
 				# check Wordnik
 				results = wordnik_api.check_all(word)
