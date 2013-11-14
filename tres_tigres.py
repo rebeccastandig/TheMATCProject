edf1ce91f8591debc59f024a024f5c753facd63c
Rebecca Standig <rebeccastandig@gmail.com>
2013-11-14 13:34:12 -0800

    Added functionality for signing in and registering.

--- tres_tigres.py
+++ tres_tigres.py
@@ -15,23 +15,34 @@ def register():
 
 @app.route("/register", methods=['POST'])
 def new_user():
+	# deal with user name __
 	name = request.form.get('name')
 	pw = request.form.get('pw')
 	verify_pw = request.form.get('verify_pw')
+	name_is_alphanum = model.check_alphanum(name)
+	pw_is_alphanum = model.check_alphanum(pw)
 
-	if (pw == verify_pw) and name:
+	if name_is_alphanum == False:
+		flash('Invalid user name. Alphanumeric characters only (A-Z, a-z, 0-9).')
+		return redirect(url_for('register'))
+	elif pw_is_alphanum == False:
+		flash('Invalid password. Alphanumeric characters only (A-Z, a-z, 0-9).')
+		return redirect(url_for('register'))
+	elif 6 > len(pw) or 15 < len(pw):
+		flash('Invalid password. Password must be between 6 and 15 characters.')
+		return redirect(url_for('register'))
+	elif pw != verify_pw:
+		flash('Passwords must match.')
+		return redirect(url_for('register'))
+	elif (pw == verify_pw) and name:
 		if model.check_if_user(name) == True:
 			flash('You\'re already registered. Please sign in.')
-			# need to create this
 			return redirect(url_for('signin'))
 		else:
 			model.set_user(name)
 			model.set_user_pw(name, pw)
 			flash('Thanks for registering! Please sign in to continue.')
 			return redirect(url_for('signin'))
-	elif pw != verify_pw:
-		flash('Passwords must match.')
-		return redirect(url_for('register'))
 	else:
 		flash('All fields are required.')
 		return redirect(url_for('register'))
@@ -43,10 +54,17 @@ def signin():
 
 @app.route("/signin", methods=['POST'])
 def sign_in():
-	# need to deal with hashing
 	name = request.form.get('name')
 	pw = request.form.get('pw')
+	name_is_alphanum = model.check_alphanum(name)
+	pw_is_alphanum = model.check_alphanum(pw)
 
-	# need to write fn in model
-	if model.auth_login(name, pw):
-		pass
+	if name_is_alphanum == True and pw_is_alphanum == True:
+		if model.auth_login(name, pw) == True:
+			flash('Sign in successful!')
+			# need to create this
+			return redirect(url_for('home'))
+	else:
+		flash('Your user name and/or password didn\'t match our records. Please try signing in again.')
+		return redirect(url_for('signin'))
+
