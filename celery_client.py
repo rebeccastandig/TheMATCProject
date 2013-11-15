47d4724c6f730f5104021c8f9f20d9d931902a13
Rebecca Standig <rebeccastandig@gmail.com>
2013-11-14 23:51:44 -0800

    Added comment.

--- celery_client.py
+++ celery_client.py
@@ -1,11 +1,13 @@
 import twython_streaming
 from celery_tasks import parse_tweets
 
+# These are the API keys for Twitter
 APP_KEY = 'secret'
 APP_SECRET = 'secret'
 
 OAUTH_TOKEN = 'secret'
 OAUTH_TOKEN_SECRET = 'secret'
+
 
 stream = twython_streaming.Streamer(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
 
