97ed7f068d14cd8e8d302011e104beb3a921e32b
Rebecca Standig <rebeccastandig@gmail.com>
2013-11-14 23:56:58 -0800

    Added functionality, now have mostly working front end.

--- tres_tigres.py
+++ tres_tigres.py
@@ -4,9 +4,11 @@ import model
 app = Flask(__name__)
 app.secret_key ='secret'
 
+#### Basic Functionality ####
+
 @app.route("/")
 def index():
-	pass
+	return render_template('index.html')
 
 @app.route("/register")
 def register():
@@ -29,6 +31,7 @@ def new_user():
 		flash('Invalid password. Alphanumeric characters only (A-Z, a-z, 0-9).')
 		return redirect(url_for('register'))
 	elif 6 > len(pw) or 15 < len(pw):
+		# add this for user name too
 		flash('Invalid password. Password must be between 6 and 15 characters.')
 		return redirect(url_for('register'))
 	elif pw != verify_pw:
@@ -62,9 +65,98 @@ def sign_in():
 	if name_is_alphanum == True and pw_is_alphanum == True:
 		if model.auth_login(name, pw) == True:
 			flash('Sign in successful!')
-			# need to create this
-			return redirect(url_for('home'))
+			session['user'] = name
+			# do something with session here
+			return redirect(url_for('index'))
 	else:
 		flash('Your user name and/or password didn\'t match our records. Please try signing in again.')
 		return redirect(url_for('signin'))
 
+@app.route("/logout")
+def logout():
+	session.clear()
+	return redirect(url_for('index'))
+
+@app.route("/about")
+def about_tt():
+	return render_template('about.html')
+
+@app.route("/corpus")
+def corpus():
+	return render_template('corpus.html')
+
+@app.route("/contact")
+def contact_me():
+	return render_template('contact_me.html')
+
+@app.route("/linguists")
+def linguist_info():
+	return render_template('info_for_linguists.html')
+
+#### End Basic Functionality ####
+
+#### Corpus Functionality ####
+
+## None of these will work until people tag in the game ##
+
+@app.route("/corpus/download")
+def corpus_download():
+	return render_template('corpus_download_main.html')
+
+@app.route("/corpus/download/pos")
+def corpus_download_pos():
+	corpus = model.get_corpus_pos()
+	# need to be able to dl this
+	return render_template('corpus_download.html', corpus = corpus)
+
+
+@app.route("/corpus/download/words")
+def corpus_download_words():
+	# need to be able to dl this
+	corpus = model.get_corpus_words()
+	return render_template('corpus_download.html', corpus = corpus)
+
+@app.route("/corpus/browse")
+def corpus_browse():
+	# i'd like to add searching ability too
+	return render_template('browse.html')
+
+@app.route("/corpus/browse/pos")
+def corpus_pos_list():
+	tag_list = model.get_pos()
+	# display clickable list of pos tags used in corpus
+	return render_template('browse_list_pos.html', corpus_list = tag_list)
+	
+@app.route("/corpus/browse/words")
+def corpus_words_list():
+	word_list = model.get_words()
+	# display clickable list of words in corpus
+	return render_template('browse_list_words.html', corpus_list = word_list)
+
+@app.route("/corpus/browse/pos/<tag>")
+def corpus_pos(tag):
+	word_list = model.get_words_by_tag(tag)
+	# display clickable list of words assoc with that tag
+	return render_template('browse_words_by_pos.html', word_list = word_list, tag = tag)
+	pass
+
+@app.route("/corpus/browse/words/<word>")
+def corpus_word(word):
+	tag_list = model.get_tags_by_word(word)
+	# display clickable list of tags assoc with that word
+	return render_template('browse_pos_by_word.html', tag_list = tag_list, word = word)
+
+#### End Corpus Functionality ####
+
+
+#### Game Functionality ####
+
+@app.route("/tres_tigres")
+def game():
+	# just for now:
+	return redirect(url_for('index'))
+
+#### End Game Functionality ####
+
+if __name__ == "__main__":
+    app.run(debug = True)
