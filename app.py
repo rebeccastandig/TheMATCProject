eca556f55e4da2ae6944d693816846af297405eb
Rebecca Standig <rebeccastandig@gmail.com>
2013-11-20 21:55:12 -0800

    Updated register and signin for redirecting back to referral

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request, flash, redirect, url_for, session
 import model
-import random
 
 app = Flask(__name__)
 app.secret_key ='secret'
@@ -12,73 +11,90 @@ def index():
 	# do something with session user in greeting
 	return render_template('index.html')
 
-@app.route("/register")
+@app.route("/register", methods=['GET', 'POST'])
 def register():
-	# do something about if already signed in
-	return render_template("register.html")
+	if request.method == 'POST':
+		name = request.form.get('name')
+		pw = request.form.get('pw')
+		verify_pw = request.form.get('verify_pw')
 
-@app.route("/register", methods=['POST'])
-def new_user():
-	name = request.form.get('name')
-	pw = request.form.get('pw')
-	verify_pw = request.form.get('verify_pw')
-
-	if len(name) > 0 and len(pw) > 0 and len(verify_pw) > 0:
-		name_is_alphanum = model.check_alphanum(name)
-		pw_is_alphanum = model.check_alphanum(pw)
-		if name_is_alphanum == False:
-			flash('Invalid user name. Alphanumeric characters only (A-Z, a-z, 0-9).')
-			return redirect(url_for('register'))
-		elif pw_is_alphanum == False:
-			flash('Invalid password. Alphanumeric characters only (A-Z, a-z, 0-9).')
-			return redirect(url_for('register'))
-		elif 6 > len(name) or 15 < len(name):
-			flash('Invalid user name. User name must be between 6 and 15 characters.')
-			return redirect(url_for('register'))
-		elif 6 > len(pw) or 15 < len(pw):
-			flash('Invalid password. Password must be between 6 and 15 characters.')
-			return redirect(url_for('register'))
-		elif pw != verify_pw:
-			flash('Passwords must match.')
-			return redirect(url_for('register'))
-		elif (pw == verify_pw) and name:
-			if model.check_if_user(name) == True:
-				flash('You\'re already registered. Please sign in.')
-				return redirect(url_for('signin'))
-			else:
-				model.set_user(name)
-				model.set_user_pw(name, pw)
-				flash('Thanks for registering! Please sign in to continue.')
-				return redirect(url_for('signin'))
-	else:
-		flash('All fields are required.')
-		return redirect(url_for('register'))
-
-@app.route("/signin")
-def signin():
-	# do something about if they're already signed in
-	return render_template('signin.html')
-
-@app.route("/signin", methods=['POST'])
-def sign_in():
-	name = request.form.get('name')
-	pw = request.form.get('pw')
-
-	if name and pw:
-		if len(name) > 0 and len(pw) > 0:
+		if len(name) > 0 and len(pw) > 0 and len(verify_pw) > 0:
 			name_is_alphanum = model.check_alphanum(name)
 			pw_is_alphanum = model.check_alphanum(pw)
-			if name_is_alphanum == True and pw_is_alphanum == True:
-				if model.auth_login(name, pw) == True:
-					flash('Sign in successful!')
-					session['user'] = name
-					return redirect(url_for('index'))
-				else:
-					flash('Your user name and/or password didn\'t match our records. Please try signing in again.')
+			if name_is_alphanum == False:
+				flash('Invalid user name. Alphanumeric characters only (A-Z, a-z, 0-9).')
+				return redirect(url_for('register'))
+			elif pw_is_alphanum == False:
+				flash('Invalid password. Alphanumeric characters only (A-Z, a-z, 0-9).')
+				return redirect(url_for('register'))
+			elif 6 > len(name) or 15 < len(name):
+				flash('Invalid user name. User name must be between 6 and 15 characters.')
+				return redirect(url_for('register'))
+			elif 6 > len(pw) or 15 < len(pw):
+				flash('Invalid password. Password must be between 6 and 15 characters.')
+				return redirect(url_for('register'))
+			elif pw != verify_pw:
+				flash('Passwords must match.')
+				return redirect(url_for('register'))
+			elif (pw == verify_pw) and name:
+				if model.check_if_user(name) == True:
+					flash('You\'re already registered. Please sign in.')
 					return redirect(url_for('signin'))
+				else:
+					model.set_user(name)
+					model.set_user_pw(name, pw)
+					flash('Thanks for registering! You\'re now signed in.')
+					session['user'] = name
+					ip_address = request.remote_addr
+					last_url = model.get_last_url(ip_address)
+					# redirect back to where they came from
+					return redirect(last_url)
+			else:
+				flash('All fields are required.')
+				return redirect(url_for('register'))
+	elif not session:
+		url = request.referrer
+		ip_address = request.remote_addr
+		model.keep_url(url, ip_address)
+		return render_template("register.html")
 	else:
-		flash('Your user name and/or password didn\'t match our records. Please try signing in again.')
-		return redirect(url_for('signin'))
+		# if there's a session
+		flash('You\'re already signed in.')
+		return redirect(request.referrer)
+	
+
+@app.route("/signin", methods=['GET', 'POST'])
+def signin():
+	if request.method =='POST':
+		name = request.form.get('name')
+		pw = request.form.get('pw')
+
+		if name and pw:
+			if len(name) > 0 and len(pw) > 0:
+				name_is_alphanum = model.check_alphanum(name)
+				pw_is_alphanum = model.check_alphanum(pw)
+				if name_is_alphanum == True and pw_is_alphanum == True:
+					if model.auth_login(name, pw) == True:
+						flash('Sign in successful!')
+						session['user'] = name
+						ip_address = request.remote_addr
+						last_url = model.get_last_url(ip_address)
+						return redirect(last_url)
+					else:
+						flash('Your user name and/or password didn\'t match our records. Please try signing in again.')
+						return redirect(url_for('signin'))
+		else:
+			flash('Your user name and/or password didn\'t match our records. Please try signing in again.')
+			return redirect(url_for('signin'))
+	elif not session:
+		url = request.referrer
+		ip_address = request.remote_addr
+		model.keep_url(url, ip_address)
+		return render_template('signin.html')
+	else:
+		# if there's a session
+		flash('You\'re already signed in.')
+		return redirect(request.referrer)
 
 @app.route("/logout")
 def logout():
